5ee49cfa866e326f847ec8f5354a92a70622db7d
DongDong107 <jkds989@gmail.com>
2023-02-01 22:28:32 +0900

    Add 20230201 Study

--- webPrj/src/main/java/com/newlecture/web/jdbc/JDBCProgram.java
+++ webPrj/src/main/java/com/newlecture/web/jdbc/JDBCProgram.java
@@ -5,12 +5,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JDBCProgram {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		
+		System.out.println("             [회원 검색 프로그램]");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("회원 이름 입력:");
+		String name = sc.nextLine(); 
+		System.out.println("ID         "+"이름        "+"별명          ");
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
@@ -20,15 +26,45 @@ public class JDBCProgram {
 		// 커넥션 관련해서 생성하면 무조건 닫는것도 만들기
 		
 		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("select * from MEMBER");
+//		ResultSet rs = st.executeQuery("select * from MEMBER where id >= 100");
+//		ResultSet rs = st.executeQuery("SELECT ID,NAME,NICNAME FROM MEMBER WHERE NAME = '"+ name +"'");
+		ResultSet rs = st.executeQuery("SELECT ID,NAME,NICNAME FROM MEMBER WHERE NAME LIKE '%"+ name +"%'");
 		
-		rs.next();
-		String nicName = rs.getString("nicname");
-		System.out.println(nicName);
+		//"SELECT ID,NAME,NICNAME FROM MEMBER WHERE NAME = '"+ name +"'";
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String dbname = rs.getString("name");
+			String nicName = rs.getString("nicname");
+			String printrs =  String.format("%d       "+"%s          "+"%s         ",id,dbname,nicName);
+			System.out.println(printrs);
+		}
 		
-		System.out.println(con.isClosed());
 		
 		
+		//rs.next() 불린 값이였다!!
+				
+//		// 필터링, 집계, 정렬
+//		while(rs.next()) {			
+//			int id = rs.getInt("id");
+//			if(id<100) {
+//				continue;
+//			}
+//			String nicName = rs.getString("nicname");
+//			
+//			System.out.println(id);
+//			System.out.println(nicName);
+//		}
+		
+		
+		
+		
+//		rs.next();
+//		String nicName = rs.getString("nicname");
+//		System.out.println(nicName);
+		
+//		System.out.println(con.isClosed());
+		
+		sc.close();
 		con.close();		
 		
 //		DriverManager
